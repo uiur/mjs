1a553b08fbd417db32c677f4c715f171ef1ac002
Kazato Sugimoto <uiureo@gmail.com>
2021-03-06 16:43:43 +0900

    use colored output

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
-#!/bin/bash -e
+#!/bin/bash
 executable=./build/main
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+RESET='\033[0m'
+
 echo "running tests..."
 echo
 for path in $(ls test/input/*.js); do
@@ -7,10 +12,10 @@ for path in $(ls test/input/*.js); do
   actual=$($executable $path)
   expected=$(cat "test/output/${name}.out")
   if [ "$expected" != "$actual" ]; then
-    echo "test failed ($path):"
+    echo -e "${RED}test failed ($path):${RESET}"
     echo "  expect: $expected"
     echo "  actual: $actual"
   else
-    echo "test passed ($path)"
+    echo -e "${GREEN}test passed ($path)${RESET}"
   fi
 done
